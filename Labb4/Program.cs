618572eb44bf048409df61fd6cbbe6420d62061d
Kamil Niescieronek <kamil.niescieronek@iths.se>
2021-03-04 14:05:35 +0100

    Added some stuff

--- Labb4/Program.cs
+++ Labb4/Program.cs
@@ -6,6 +6,9 @@ namespace Labb4
 {
     class Program
     {
+        public static readonly string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public static readonly string folder = Path.Combine(appDataPath, @"Labb4WorkShopApp");
+
         static void Main(string[] args)
         {
             //Console Applcation
@@ -14,10 +17,10 @@ namespace Labb4
             in olika argument till programmet. Om man inte skickar några argument (eller
             felaktiga argument) ska följande skrivas ut: */
 
-            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            var path = Path.Combine(appDataPath, @"Labb4WorkShopApp");                      //<-- DON'T Forget to change this after done with draft
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var folder = Path.Combine(appDataPath, @"Labb4WorkShopApp");                      //<-- DON'T Forget to change this after done with draft
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
 
             Console.WriteLine("Use any of the following parameters:");
             Console.WriteLine("-lists");
